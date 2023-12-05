493ef45066511eab6d12ba1224f8d96162b36df6
FranJpann <loann.cady49@gmail.com>
2023-12-05 16:14:08 +0100

    je me permet un peu de doc ..........

--- src/main/PublicKey.java
+++ src/main/PublicKey.java
@@ -3,18 +3,11 @@ import java.util.Random;
 
 public class PublicKey {
 
-    // p et q grands entiers premiers
-    // p != q
+
     private BigInteger p;
     private BigInteger q;
-
-    // n = p x q
     private BigInteger n;
-
-    // m -> indice euler, m = (p-1) x (q-1)
     private BigInteger m;
-
-    // e -> exposant public
     private BigInteger e;
 
     public BigInteger getP() {
@@ -38,10 +31,19 @@ public class PublicKey {
     }
 
     PublicKey(){
+
+        // p et q grands entiers premiers
+        // p != q
         this.p=BigInteger.probablePrime(2048, new Random());
         this.q=this.p.nextProbablePrime();
+
+        // n = q x p
         this.n=this.q.multiply(this.p);
+
+        // m est égal au nombre d'entiers naturels <= n
         this.m=(this.p.subtract(BigInteger.valueOf(1)).multiply(this.q.subtract(BigInteger.valueOf(1))));
+
+        // e un nombre aléatoire premier à m
         this.e=new BigInteger(512,new Random());
         if(this.e.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))){
             this.e=this.e.subtract(BigInteger.valueOf(1));

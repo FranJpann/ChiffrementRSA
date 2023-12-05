f1bb09612f6d1422eb1f2e2d8226cf5bbc0a266c
FranJpann <loann.cady49@gmail.com>
2023-12-05 15:12:39 +0100

    ajout thread serveur tcp

--- src/main/tcp/ServeurTCP.java
+++ src/main/tcp/ServeurTCP.java
@@ -7,8 +7,9 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServeurTCP {
+public class ServeurTCP extends Thread{
   final static int port = 9632;
+  private Socket socket;
 
   public static void main(String[] args) {
     try {
@@ -17,23 +18,35 @@ public class ServeurTCP {
 
       while (true) {
         Socket socketClient = socketServeur.accept();
-        String message = "";
-
-        System.out.println("Connexion avec : "+socketClient.getInetAddress());
-
-        InputStream in = socketClient.getInputStream();
-        // OutputStream out = socketClient.getOutputStream();
-
-        BufferedReader input = new BufferedReader(
-          new InputStreamReader(in));
-        PrintStream out = new PrintStream(socketClient.getOutputStream());
-        message = input.readLine();
-
-        System.out.println(message);
-
-        socketClient.close();
+        ServeurTCP serveurTCP = new ServeurTCP(socketClient);
+        serveurTCP.start();
       }
     } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public ServeurTCP(Socket socket) {
+    this.socket = socket;
+  }
+
+  public void run() {
+    try {
+      String message = "";
+
+      System.out.println("Connexion avec : "+socket.getInetAddress());
+
+      InputStream in = socket.getInputStream();
+
+      BufferedReader input = new BufferedReader(
+              new InputStreamReader(in));
+      PrintStream out = new PrintStream(socket.getOutputStream());
+      message = input.readLine();
+
+      System.out.println(message);
+
+      socket.close();
+    }catch (Exception e){
       e.printStackTrace();
     }
   }

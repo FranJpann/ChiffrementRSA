a213e8726b88dc0d6ceb3b3b91e080eea5f83951
FranJpann <loann.cady49@gmail.com>
2023-12-05 14:34:52 +0100

    fix structure + serveurTCP

--- src/main/ServeurTCP.java
+++ src/main/ServeurTCP.java
@@ -1,4 +1,10 @@
-public class TestServeurTCP {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServeurTCP {
   final static int port = 9632;
 
   public static void main(String[] args) {

8b094cd6d344e087636109a13a6def25f6f2f20c
Anthony-Guillot <guillot.anthony01@gmail.com>
2024-01-24 09:29:30 +0100

    Chiffrement et Dechifrrement

--- src/main/Chiffrement.java
+++ src/main/Chiffrement.java
@@ -1,13 +1,26 @@
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Chiffrement {
     private BigInteger[] chiffre;
 
-    Chiffrement(String str,BigInteger p,BigInteger m){
+    public BigInteger[] getChiffre() {
+        return chiffre;
+    }
+
+    Chiffrement(String str, BigInteger e, BigInteger n){
+        System.out.println("Chiffrement -> Begin");
         this.chiffre=new BigInteger[str.length()];
         for(int i=0;i<str.length();i++){
-            chiffre[i]=BigInteger.valueOf((int)str.charAt(i)).modPow(p,m);
+            chiffre[i]=BigInteger.valueOf((int)str.charAt(i)).modPow(e,n);
         }
+    }
+
+
+
+    public static void main(String[] args) {
+        Chiffrement test=new Chiffrement("Ceci est un test",new BigInteger(50,new Random()),new BigInteger(50,new Random()));
+        System.out.println(Arrays.toString(test.chiffre));
     }
 }

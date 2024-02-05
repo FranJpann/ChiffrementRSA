f7bad2d25e4eeee6c11a4c392566656983e19ee2
FranJpann <loann.cady@gmail.com>
2024-02-05 22:03:28 +0100

    scénario bob

--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -7,6 +7,9 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        // PeerToPeerClient peerToPeerClient = new PeerToPeerClient("Bob");
+
         Socket socket = new Socket("192.168.1.34", 12345);
         PeerToPeerClient peerToPeerClient = new PeerToPeerClient("Alice");
         peerToPeerClient.sendPublicKey(socket);

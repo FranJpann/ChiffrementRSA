93ec2af23798141b39b70ba7dffebb4cf3a23943
FranJpann <loann.cady@gmail.com>
2024-02-05 23:35:50 +0100

    ajout attente 3secondes pour la réception de la clé publique

--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -10,15 +10,16 @@ import java.math.BigInteger;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // PeerToPeerClient peerToPeerClient = new PeerToPeerClient("Bob");
 
         Socket socket = new Socket("192.168.1.34", 12345);
         PeerToPeerClient peerToPeerClient = new PeerToPeerClient("Alice");
-        peerToPeerClient.sendPublicKey(socket);
-        peerToPeerClient.sendMessage(socket, "Ceci un message déchiffré", "Bob");
 
+        peerToPeerClient.sendPublicKey(socket, true);
+        Thread.sleep(3000);
+        peerToPeerClient.sendMessage(socket, "Ceci un message déchiffré", "Bob");
     }
 
      /* Exemple d'utilisation du chiffrement / déchiffrement
